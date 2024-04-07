b38cad3968abc92d48d53243f20c8985a983259f
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-07 12:15:24 +0900

    first commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by 川口美咲 on 2024/04/07.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
