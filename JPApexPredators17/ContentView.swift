34105d926c56c0c975760a1ef8412038f762f73a
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-08 19:45:31 +0900

    typeでフィルターする機能を追加

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State var searchText = ""
     @State var alphabetical = false
+    @State var currentSelection = PredatorType.all
     
     let predators = Predators()
     
     var filteredDinos: [ApexPredator] {
+        predators.filter(by: currentSelection)
         predators.sort(by: alphabetical)
         return predators.search(for: searchText)
     }
@@ -64,6 +66,17 @@ struct ContentView: View {
                     } label: {
                         Image(systemName: alphabetical ? "film" : "textformat")
                             .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection: $currentSelection) {
+                            ForEach(PredatorType.allCases) { type in
+                                Label(type.rawValue.capitalized, systemImage: type.icon)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
                     }
                 }
             }
